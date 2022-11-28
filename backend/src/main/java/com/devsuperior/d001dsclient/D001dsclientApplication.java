c3a2897994c08ea8da6c68b4137d8b93a6e3d1d1
Reginaldo B. F <reginaldobf21@gmail.com>
2022-11-27 21:26:17 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/d001dsclient/D001dsclientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.d001dsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D001dsclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D001dsclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/d001dsclient/D001dsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.d001dsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D001dsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
